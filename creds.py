afd62ac504a35acf765eb51f1cc1d3fa925c504d
Lonnie Rodrigue <rodriguel776@students.cochise.edu>
2021-05-17 19:39:15 +0530

    upoated

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "1859969304:AAGaVnhdDY37L9axFMbctlSyFx_VN1mW9-g"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1pBtOzFcM8kr-5EyuNWdyQzwg1Dy3nVRW"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AMGt-gpLGrkPUk9PVA"
     
     
     
